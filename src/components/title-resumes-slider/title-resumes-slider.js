a294f834c16eecd0792c1c8cf6fe0accef30d6f1
emy-wins <benkhadra.iman@gmail.com>
2024-09-26 15:11:48 +0200

    #update-

---
+++ src/components/title-resumes-slider/title-resumes-slider.js
@@ -0,0 +1,13 @@
+$(".slider-resumes").slick({
+  variableWidth: true,
+  centerMode: true,
+  arrows: true,
+  dots: false,
+  speed: 300,
+  centerPadding: '100px',
+  infinite: true,
+  autoplaySpeed: 3000,
+  autoplay: true,
+  prevArrow: '.prev',
+  nextArrow: '.next',
+});
