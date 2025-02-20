e97b029b60960b3d952f2e0035314432cce59427
emy-wins <benkhadra.iman@gmail.com>
2025-02-20 15:45:47 +0100

    update

--- src/components/tabs-form/tabs-form.js
+++ src/components/tabs-form/tabs-form.js
@@ -58,44 +58,35 @@ $(document).ready(function () {
     $(".cv-bg").css('background-color', selectedColor);
   });
 
-  // $(".dwn").on("click", function () {
-  //   window.jsPDF = window.jspdf.jsPDF;
-  //   var docpdf = new jsPDF();
-  //   var elementHTML = document.querySelector("#cv");
-  //   var invoiceNo = "MyResume";
-  //   const pageWidth = docpdf.internal.pageSize.getWidth();
-  //   docpdf.setFont("helvetica", "bold");
-  //   docpdf.html(elementHTML, {
-  //     callback: function () {
-  //       docpdf.setFont("helvetica", "bold");
-  //       docpdf.save(invoiceNo + '.pdf');
-  //     },
-  //     x: 0,
-  //     y: 0,
-  //     width: pageWidth,
-  //     windowWidth: elementHTML.offsetWidth,
-  //   });
-
-  // });
-
-
   $(".dwn").on("click", function () {
+    // Assume we have an SVG element in our HTML
+    let svgElem = document.querySelector("#vectorSVG");
+
+    // Convert SVG to XML
+    let serializer = new XMLSerializer();
+    let source = serializer.serializeToString(svgElem);
+
+    // Convert XML to Image and draw on canvas
+    let img = new Image();
+    img.src = "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(source)));
+    let canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // // Convert canvas to PDF (using the hypothetical saveAsPDF function)
+    // canvas.saveAsPDF('output.pdf');
 
     window.jsPDF = window.jspdf.jsPDF;
-    var pdf = new jsPDF({
-      orientation: "portrait",
-      unit: "pt",
-      format: "a4",
-    });
-
-    const svgElement = document.getElementById("vectorSVG");
-    const pageWidth = pdf.internal.pageSize.getWidth();
-    svg2pdf(svgElement, pdf, { x: 10, y: 10, width: pageWidth });
-
-    pdf.setFont(undefined, 'normal')
-    pdf.save("vector-svg.pdf");
+    const pdf = new jsPDF("portrait", "mm", "a4"); // Create PDF document
+    const imgWidth = 210; // A4 width in mm
+    const imgHeight = (canvas.height / canvas.width) * imgWidth; // Maintain aspect ratio
+    pdf.addImage(canvas, "PNG", 0, 0, imgWidth, imgHeight); // Add image to PDF
+    pdf.save("canvas.pdf");
+    img.onload = function () {
+    };
   });
-
 
 })
 
@@ -103,32 +94,4 @@ $(document).ready(function () {
 
 
 
-// Assume we have an SVG element in our HTML
-let svgElem = document.querySelector("#vectorSVG");
 
-// Convert SVG to XML
-let serializer = new XMLSerializer();
-let source = serializer.serializeToString(svgElem);
-
-// Convert XML to Image and draw on canvas
-let img = new Image();
-img.src = "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(source)));
-console.log(img.src);
-img.onload = function () {
-  let canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  let ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-
-  // // Convert canvas to PDF (using the hypothetical saveAsPDF function)
-  // canvas.saveAsPDF('output.pdf');
-
-  window.jsPDF = window.jspdf.jsPDF;
-  const pdf = new jsPDF("portrait", "mm", "a4"); // Create PDF document
-  const imgWidth = 210; // A4 width in mm
-  const imgHeight = (canvas.height / canvas.width) * imgWidth; // Maintain aspect ratio
-
-  pdf.addImage(canvas, "PNG", 0, 0, imgWidth, imgHeight); // Add image to PDF
-  pdf.save("canvas.pdf"); // Save the PDF
-};
